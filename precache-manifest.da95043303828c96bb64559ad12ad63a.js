cd02b1059d3999c7745d62ddb1fbb37083f4095c
Ruiz Ruiz,Jose <Jose.RuizRuiz@Cerner.com>
2019-11-07 13:16:17 +0000

    Updates

---
+++ precache-manifest.da95043303828c96bb64559ad12ad63a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db953955784a46a5f96cac5fe801caa",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f3c38defb6e9d7149107",
+    "url": "/tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "14d01948d64218d7a575",
+    "url": "/tic-tac-toe/static/js/2.4dd71385.chunk.js"
+  },
+  {
+    "revision": "f3c38defb6e9d7149107",
+    "url": "/tic-tac-toe/static/js/main.218b381a.chunk.js"
+  },
+  {
+    "revision": "385629666264212be688",
+    "url": "/tic-tac-toe/static/js/runtime-main.9b00db00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c73e83c2b639bc420770196dc9297834.js"
+  "/tic-tac-toe/precache-manifest.da95043303828c96bb64559ad12ad63a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac-toe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
